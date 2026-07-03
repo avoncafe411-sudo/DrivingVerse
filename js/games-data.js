65f9d6f389704e6222affb805f66fbdcd79b7d56
avoncafe411-sudo <avoncafe411@gmail.com>
2026-07-03 19:52:39 +0530

    feat: add games data for Extreme Car Driving Simulator

---
+++ js/games-data.js
@@ -0,0 +1,35 @@
+window.games = [
+{
+    id: "54750",
+
+    title: "Extreme Car Driving Simulator",
+
+    slug: "extreme-car-driving-simulator",
+
+    category: "Racing",
+
+    rating: "4.8",
+
+    description: "Drive powerful sports cars, complete parking missions, transport passengers and explore a huge open world.",
+
+    instructions: "Mouse click or tap to play.",
+
+    image: "https://img.gamemonetize.com/wtmr1ws4khywnki9fm53csa0txss21tb/512x384.jpg",
+
+    url: "https://html5.gamemonetize.co/wtmr1ws4khywnki9fm53csa0txss21tb/",
+
+    featured: true,
+
+    trending: true,
+
+    recent: true,
+
+    popular: true,
+
+    buttonText: "Play Now",
+
+    detailUrl: "games/extreme-car-driving-simulator.html",
+
+    link: "games/extreme-car-driving-simulator.html"
+}
+];
