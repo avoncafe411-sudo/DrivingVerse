e7d9d9745f224d59caa3e507d7695a0d28c35300
avoncafe411-sudo <avoncafe411@gmail.com>
2026-07-04 19:07:34 +0530

    feat: add Car Driving 3D Champ 2024 game with HTML structure and gameplay functionality

--- js/games-data.js
+++ js/games-data.js
@@ -32,5 +32,42 @@ window.games = [
     instructions: "Mouse click or tap to play",
 
     tags: "Car"
-}
+},
+
+{
+    id: "57313",
+
+    slug: "car-driving-3d-champ-2024",
+
+    title: "Car Driving 3D Champ 2024",
+
+    category: "Racing",
+
+    rating: "4.7",
+
+    description: "Get ready to burn rubber and conquer the bustling city streets in this thrilling online car driving game.",
+
+    image: "https://img.gamemonetize.com/ic8o64eot1bewdh0e2l8b0ong4y9j1q0/512x384.jpg",
+
+    featured: true,
+    trending: true,
+    recent: true,
+    popular: true,
+
+    buttonText: "Play Now",
+
+    link: "games/car-driving-3d-champ-2024.html",
+
+    detailUrl: "games/car-driving-3d-champ-2024.html",
+
+    url: "https://html5.gamemonetize.co/ic8o64eot1bewdh0e2l8b0ong4y9j1q0/",
+
+    width: "600",
+    height: "600",
+
+    instructions: "Keyboard Arrows - Up, Down, Left, Right. Space Bar for Handbrake.",
+
+    tags: "3D, Cars, Racing"
+},
 ];
+
