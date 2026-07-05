b36faf42d2a3993862b80d2f1716ebcf9e60c471
avoncafe411-sudo <avoncafe411@gmail.com>
2026-07-05 15:08:29 +0530

    feat: add HTML structure and gameplay functionality for Hill Climb Racings 2 and Offroad Truck Driving Simulator

--- js/games-data.js
+++ js/games-data.js
@@ -143,6 +143,76 @@ window.games = [
     instructions: "Use intuitive on-screen controls to drive your vehicle smoothly. Tap left and right buttons to steer, switch camera views, and activate the booster button for an instant speed boost.",
 
     tags: "Truck"
+},
+
+{
+    id: "78892",
+    slug: "hill-climb-racings-2",
+
+    title: "HIll climb Racings 2",
+
+    category: "Racing",
+
+    rating: "4.8",
+
+    description: "Hill Climb Racings 2 is back, bigger, better and it's even more fun! Join millions of players around the world in this realistic physics racing game.",
+
+    image: "https://img.gamemonetize.com/1vxk8rfmkpin0l0ex7atiuftp6l3mu6p/512x384.jpg",
+
+    featured: true,
+    trending: true,
+    recent: true,
+    popular: true,
+
+    buttonText: "Play Now",
+
+    link: "games/hill-climb-racings-2.html",
+
+    detailUrl: "games/hill-climb-racings-2.html",
+
+    url: "https://html5.gamemonetize.co/1vxk8rfmkpin0l0ex7atiuftp6l3mu6p/",
+
+    width: "480",
+    height: "800",
+
+    instructions: "Mouse click or tap to play",
+
+    tags: "Adventure"
+},
+
+{
+    id: "76079",
+    slug: "offroad-truck-driving-simulator",
+
+    title: "Offroad Truck Driving Simulator",
+
+    category: "Racing",
+
+    rating: "4.8",
+
+    description: "Offroad Truck Driving Simulator is the ultimate off-road mud truck driving game with realistic physics, powerful 4x4 trucks, and exciting off-road challenges.",
+
+    image: "https://img.gamemonetize.com/bwrunfiqbjfxprp91bq5v0xhtqzmukno/512x384.jpg",
+
+    featured: true,
+    trending: true,
+    recent: true,
+    popular: true,
+
+    buttonText: "Play Now",
+
+    link: "games/offroad-truck-driving-simulator.html",
+
+    detailUrl: "games/offroad-truck-driving-simulator.html",
+
+    url: "https://html5.gamemonetize.co/bwrunfiqbjfxprp91bq5v0xhtqzmukno/",
+
+    width: "800",
+    height: "600",
+
+    instructions: "W A S D or Arrow Keys - Move Truck | H - Horn | R - Reverse",
+
+    tags: "Truck"
 }
 ];
 
